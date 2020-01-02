1151a52361bb3dea46c7c69d51682212875ed2b7
Nikola Vukotic <nikolaraca@elfak.rs>
2020-01-03 00:40:48 +0100

    started adding models

---
+++ API/TennisOpenQuizServer/TennisOpenQuizCashing/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisOpenQuizCashing.Models
+{
+    public class Question
+    {
+        public Question()
+        {
+
+        }
+
+        public string QuestionContent { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
---
+++ API/TennisOpenQuizServer/TennisOpenQuizCashing/Models/QuestionAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisOpenQuizCashing.Models
+{
+    public class QuestionAnswer
+    {
+        public QuestionAnswer()
+        {
+
+        }
+        public string QuestionID { get; set; }
+        public string AnswerID { get; set; }
+        public string UserID { get; set; }
+    }
+}
