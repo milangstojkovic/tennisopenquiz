b4fc81681bd3e0cfe213c31b5428f477d2802c4b
milangstojkovic <49944962+milangstojkovic@users.noreply.github.com>
2020-01-03 00:30:45 +0100

    CassandraServices1

---
+++ API/TennisOpenQuizServer/TennisOpenQuizAPI/Services/PlayerService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cassandra;
+using TennisOpenQuizAPI.Models;
+
+namespace TennisOpenQuizAPI.Services
+{
+    public class PlayerService
+    {
+        public static List<Player> GetPlayers()
+        {
+            ISession session = SessionManager.GetSession();
+            List<Player> playersList = new List<Player>();
+            if (session == null)
+                return null;
+            var playersData = session.Execute("select * from \"Player\"");
+            foreach (var playerData in playersData)
+            {
+                Player player = new Player();
+                player.PlayerID = playerData["PlayerID"] != null ? playerData["PlayerID"].ToString() : string.Empty;
+                player.Name = playerData["Name"] != null ? playerData["Name"].ToString() : string.Empty;
+                player.Surname = playerData["Surname"] != null ? playerData["Surname"].ToString() : string.Empty;
+                player.Ranking = playerData["Ranking"] != null ?Int32.Parse(playerData["Ranking"].ToString()) : 0;
+                player.Score = playerData["Score"] != null ? Int32.Parse(playerData["Score"].ToString()) : 0;
+                player.BirthDate = playerData["BirthDate"] != null ? DateTime.Parse(playerData["BirthDate"].ToString()) : DateTime.MinValue;
+                playersList.Add(player);
+            }
+            return playersList;
+        }
+        public static Player GetPlayer(string playerID)
+        {
+            ISession session = SessionManager.GetSession();
+            if (session == null)
+                return null;
+            var playerData = session.Execute("select * from \"Player\" where \"PlayerID\" = '"+playerID+"'").FirstOrDefault();
+            Player player = new Player();
+            if (playerData != null)
+            {
+                player.PlayerID = playerData["PlayerID"] != null ? playerData["PlayerID"].ToString() : string.Empty;
+                player.Name = playerData["Name"] != null ? playerData["Name"].ToString() : string.Empty;
+                player.Surname = playerData["Surname"] != null ? playerData["Surname"].ToString() : string.Empty;
+                player.Ranking = playerData["Ranking"] != null ? Int32.Parse(playerData["Ranking"].ToString()) : 0;
+                player.Score = playerData["Score"] != null ? Int32.Parse(playerData["Score"].ToString()) : 0;
+                player.BirthDate = playerData["BirthDate"] != null ? DateTime.Parse(playerData["BirthDate"].ToString()) : DateTime.MinValue;
+            }
+            return player;
+        }
+
+        public static void AddPlayer(Player player)
+        {
+            ISession session = SessionManager.GetSession();
+            if (session == null)
+                return;
+            RowSet userData = session.Execute("insert into \"Player\" (\"PlayerID\", Name, Surname, Ranking, Score, BirthDate)  values ('" + player.PlayerID + "', '" + player.Name + "', '" + player.Surname + "', '" + player.Ranking + "', '"+player.Score+"', '"+player.BirthDate+"')");
+        }
+    }
+}
