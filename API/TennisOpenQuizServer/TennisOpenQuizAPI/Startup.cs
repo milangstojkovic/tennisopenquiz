30829d2fd5605330fa2343dc2c59e6aa096c7455
Nikola Vukotic <nikolaraca@elfak.rs>
2020-01-08 16:53:16 +0100

    cors policy cassandra

--- API/TennisOpenQuizServer/TennisOpenQuizAPI/Startup.cs
+++ API/TennisOpenQuizServer/TennisOpenQuizAPI/Startup.cs
@@ -19,6 +19,12 @@ namespace TennisOpenQuizAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +38,7 @@ namespace TennisOpenQuizAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("MyPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
