89943737aeee6a3d60b27c514c33ee2ab120b41e
milangstojkovic <49944962+milangstojkovic@users.noreply.github.com>
2020-02-06 14:06:23 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Questions from "./Components/QuestionsComponent/questions";
-import AdminHome from "./Components/AdminComponent/adminHome";
-import Register from "./Components/RegisterComponent/register";
-import Login from "./Components/LoginComponent/login";
-import AdminStatistic from "./Components/AdminStatistic/adminStatistic";
 import NavComponent from "./Components/NavComponent/navComponent";
-import { updateUserService } from "./CassandraServices/user.service";
-import {User}from "./Models/Model";
-import MatchList from "./Components/MatchListComponent/matchList";
-import ClientMatch from "./Components/ClientMatchComponent/clientMatch";
+import Home from "./Components/HomeComponent/home";
 export type Props = {};
 interface IState {
   nmbr: number;
@@ -33,22 +25,10 @@ class App extends Component<Props, IState> {
         <div className="header">
           <NavComponent />
         </div>
-        <div className="container">
-          {/* <div>
-          {this.state.nmbr > 3 ? <Login /> : <Home />}
-          <button
-            onClick={ev => this.clickedButtonCancel(ev)}
-            className="meetup-cancel-button"
-          >
-            Click me
-          </button>
-        </div>
-        <Register /> */}
-          <ClientMatch />
-        <AdminStatistic key={1} matchId="caos" />
-          <AdminHome />
-          <Questions />
-        </div>
+      <div className="container">
+
+    <Home/>
+    </div>
       </div>
     );
   }
